f62433a3a8aca355f3a5ced63a770eccc3479003
kalyanchittala <114063986+kalyanchittala@users.noreply.github.com>
2022-10-21 16:25:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -12,13 +12,3 @@ def newDeploy(jobname,ip,context)
 {
   sh "scp -r /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
 }
-
-def runSalenium(jobname)
-{
-  sh 'java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar'
-}
-
-def newDeploy(jobname,ip,context)
-{
-  sh "scp -r /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
-}
